29a36468dc6fc21238696f990966a5b88144f122
grfoger <grfoger@gmail.com>
2023-02-02 07:49:23 +0300

    init

---
+++ src/main/java/ru/job4j/url_shortcut/Job4jUrlShortcutApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.url_shortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jUrlShortcutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jUrlShortcutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/url_shortcut/Job4jUrlShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.url_shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jUrlShortcutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
