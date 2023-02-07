2b47f9b13dbf93dc0a94104672f36af5658e82e2
Arhip <grfoger@gmail.com>
2023-02-07 15:49:27 +0300

    remove wrong todo add generation login and pass

--- src/main/java/ru/job4j/shortcut/service/SimpleUserService.java
+++ src/main/java/ru/job4j/shortcut/service/SimpleUserService.java
@@ -1,6 +1,7 @@
 package ru.job4j.shortcut.service;
 
 import lombok.AllArgsConstructor;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.stereotype.Service;
 import ru.job4j.shortcut.model.User;
 import ru.job4j.shortcut.repository.UserRepository;
@@ -17,11 +18,14 @@ public class SimpleUserService implements UserService{
     private final UserRepository users;
     private final AtomicInteger accountDataGenerator = new AtomicInteger();
 
+    private final static int LOGIN_LENGTH = 6;
+    private final static int PASS_LENGTH = 8;
+
     @Override
     public Optional<User> save(User user) {
         int current = accountDataGenerator.getAndIncrement();
-        user.setLogin("user" + current);
-        user.setPassword("pass" + current);
+        user.setLogin(RandomString.make(LOGIN_LENGTH));
+        user.setPassword(RandomString.make(PASS_LENGTH));
         return Optional.of(users.save(user));
     }
 
