22862e20cada07c3abc2cf0a676ef5148074265b
grfoger <grfoger@gmail.com>
2023-02-18 16:22:08 +0300

    rewrite tests to Junit5

--- src/test/java/ru/job4j/shortcut/controller/UserControllerTest.java
+++ src/test/java/ru/job4j/shortcut/controller/UserControllerTest.java
@@ -1,35 +1,29 @@
 package ru.job4j.shortcut.controller;
-
-import org.assertj.core.internal.bytebuddy.asm.Advice;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import ru.job4j.shortcut.UrlShortCutApp;
 import ru.job4j.shortcut.model.User;
 import ru.job4j.shortcut.service.UserService;
 
-import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.Mockito.verify;
 
-@RunWith(SpringRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = UrlShortCutApp.class)
 @AutoConfigureMockMvc
 public class UserControllerTest {
--- src/test/java/ru/job4j/shortcut/service/SimpleUserServiceTest.java
+++ src/test/java/ru/job4j/shortcut/service/SimpleUserServiceTest.java
@@ -1,23 +1,21 @@
 package ru.job4j.shortcut.service;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.job4j.shortcut.UrlShortCutApp;
-import ru.job4j.shortcut.configuration.DataSourceConfiguration;
 import ru.job4j.shortcut.configuration.LiquibaseConfiguration;
 import ru.job4j.shortcut.model.User;
 import ru.job4j.shortcut.repository.UserRepository;
 
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {UrlShortCutApp.class, LiquibaseConfiguration.class})
 public class SimpleUserServiceTest {
     @Autowired
@@ -25,22 +23,24 @@ public class SimpleUserServiceTest {
     @Autowired
     public SimpleUserService userService;
 
-    @Test (expected = DataIntegrityViolationException.class)
+    @Test
     public void whenSaveUniqueField() {
-        User user1 = new User(1, "www.1.ru", "user1", "pass");
-        User user2 = new User(2, "www.2.ru", "user2", "pass");
-        users.save(user1);
-        users.save(user2);
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            User user1 = new User(1, "http://www.1.ru", "user1", "pass");
+            User user2 = new User(2, "http://www.2.ru", "user2", "pass");
+            users.save(user1);
+            users.save(user2);
+        });
     }
 
     @Test
     public void whenUseSaveService() {
         User user = new User();
-        user.setUrl("example");
+        user.setUrl("http://example.ru");
         Optional<User> dbUser = userService.save(user);
         users.deleteById(dbUser.get().getId());
-        Assert.assertNotNull(dbUser.get().getLogin());
-        Assert.assertNotNull(dbUser.get().getPassword());
-        Assert.assertEquals(user.getUrl(), dbUser.get().getUrl());
+        assertNotNull(dbUser.get().getLogin());
+        assertNotNull(dbUser.get().getPassword());
+        assertEquals(user.getUrl(), dbUser.get().getUrl());
     }
 }
